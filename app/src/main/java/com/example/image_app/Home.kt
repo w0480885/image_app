967ad0b1062e040ed28f06d46be797f9dd63f876
w0480885 <w0480885@nscc.ca>
2024-04-05 18:13:43 -0300

    first change

---
+++ app/src/main/java/com/example/image_app/Home.kt
@@ -0,0 +1,39 @@
+package com.example.image_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.image_app.databinding.FragmentHomeBinding
+
+class Home : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCats.setOnClickListener {
+            activity?.let {
+                startActivity(Intent(it, CatActivity::class.java))
+            }
+        }
+        binding.btnDogs.setOnClickListener {
+            activity?.let {
+                startActivity(Intent(it, DogActivity::class.java))
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
